2e152abf9628ec4f0d2f4f4f2b56cece666cd404
terabit <amiranaisanov@gmail.com>
2026-09-04 19:42:30 +0300

    Fixed a bug with copying photos and files from the launcher.

--- quickshell/launcher/copy.sh
+++ quickshell/launcher/copy.sh
@@ -2,17 +2,41 @@
 
 set -eu
 
-# Убираем пробелы и переводы строк из ID
 id=$(printf '%s' "$1" | tr -d '[:space:]')
-value=$"$2"
+value="${2:-}"
 
 if [ -z "$id" ]; then
     exit 1
 fi
 
+# Получаем содержимое записи
+content=$(printf '%s' "$id" | cliphist decode)
+
+# -------------------------------------
+# FILE
+# -------------------------------------
+
+if printf '%s\n' "$content" | grep -q '^file://'; then
+
+    printf '%s\n' "$content" |
+        wl-copy --type text/uri-list
+
+    notify-send \
+        -u low \
+        "File copied" \
+        "${value:-File}"
+
+    exit 0
+fi
+
+# -------------------------------------
+# IMAGE
+# -------------------------------------
+
 entry=$(cliphist list | grep "^${id}[[:space:]]" || true)
 
 if printf '%s\n' "$entry" | grep -q '\[\[ binary data '; then
+
     format=$(printf '%s\n' "$entry" |
         sed -n 's/.* \([a-zA-Z0-9+.-]*\) [0-9][0-9]*x[0-9][0-9]* \]\]$/\1/p')
 
@@ -41,11 +65,22 @@ if printf '%s\n' "$entry" | grep -q '\[\[ binary data '; then
         cliphist decode |
         wl-copy --type "$mime"
 
-    notify-send -u low "Image copied" "Clipboard"
-else
-    printf '%s' "$id" |
-        cliphist decode |
-        wl-copy --type text/plain
+    notify-send \
+        -u low \
+        "Image copied" \
+        "Clipboard"
 
-    notify-send -u low "Copied: '$2'" "Clipboard" 
+    exit 0
 fi
+
+# -------------------------------------
+# TEXT
+# -------------------------------------
+
+printf '%s' "$content" |
+    wl-copy --type text/plain
+
+notify-send \
+    -u low \
+    "Copied" \
+    "${value:-Clipboard}"
